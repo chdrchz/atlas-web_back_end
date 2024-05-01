b75a9e910d4227c8ab626fefae1fe8a6a6ed6515
chdrchz <saavannaa.rose@gmail.com>
2024-05-01 14:08:00 -0500

    making it executable

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,8 +1,5 @@
 export default class ClassRoom {
     constructor(maxStudentsSize) {
-        if (typeof maxStudentsSize !== number) {
-            throw new error('Not a number');
-        }
         this._maxStudentsSize = maxStudentsSize;
     }
 }
