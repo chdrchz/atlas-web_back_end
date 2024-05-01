e564c5393c837ae19795933253bb6dab0518f11a
chdrchz <saavannaa.rose@gmail.com>
2024-05-01 14:29:30 -0500

    function should be outside of class definition, and added export function to the beginning

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -3,13 +3,14 @@ export default class ClassRoom {
     constructor(maxStudentsSize) {
       this._maxStudentsSize = maxStudentsSize;
     }
+}
 
-    //initialize new rooms with a specific size 
-    initializeRooms() {
-        const rooms = [
-            new ClassRoom(19),
-            new ClassRoom(20),
-            new ClassRoom(34),
-        ];
-    }
-  }
+//initialize new rooms with a specific size 
+export function initializeRooms() {
+    const rooms = [
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(34),
+    ];
+    return rooms;
+}
