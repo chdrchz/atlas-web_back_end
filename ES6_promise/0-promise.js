1620dbf45b2418e0bc8aed9a8fd1b03b3f142f31
chdrchz <saavannaa.rose@gmail.com>
2024-05-01 11:48:48 -0500

    trying again?

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
 function getResponseFromAPI() {
-    return Promise.resolve(true);
+    const p = new Promise((resolve, reject) => {
+        let num = 1 + 1
+        if (num == 2) {
+            resolve('Success')
+        } else {
+            reject('Failure')
+        }
+    });
 }
