21293b4c686ef95507696ce1b5e91370a855133b
chdrchz <saavannaa.rose@gmail.com>
2024-05-01 14:00:50 -0500

    adding error to make sure it is a number

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,8 @@
 export default class ClassRoom {
     constructor(maxStudentsSize) {
+        if (typeof maxStudentsSize !== number) {
+            throw new error('Not a number');
+        }
         this.__maxStudentsSize = maxStudentsSize;
     }
 }
