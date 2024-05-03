18b5abce5107e40120a102349e780f10d3d9dfe3
chdrchz <saavannaa.rose@gmail.com>
2024-05-03 17:30:14 -0500

    removing spaces before function arguemnts to pass linter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,6 +1,6 @@
 export default class HolbertonCourse {
   // constructor method for obj creation
-  constructor (name, length, students) {
+  constructor(name, length, students) {
     // verify name
     if (typeof name !== 'string' || name.trim().length === 0) {
       throw new Error('Name must be a non-empty string');
@@ -22,34 +22,34 @@ export default class HolbertonCourse {
   }
 
   // getters
-  get name () {
+  get name() {
     return this._name;
   }
 
-  get length () {
+  get length() {
     return this._length;
   }
 
-  get students () {
+  get students() {
     return this._students;
   }
 
   // setters
-  set name (newName) {
+  set name(newName) {
     if (typeof newName !== 'string' || newName.trim().length === 0) {
         throw new Error('Name must be a non-empty string');
     }
     this._name = newName;
   }
 
-  set length (newLength) {
+  set length(newLength) {
     if (typeof newLength !== 'number' || newLength <= 0) {
         throw new Error('Length must be a positive number');
     }
     this._length = newLength;
   }
 
-  set students (newStudents) {
+  set students(newStudents) {
     if (!Array.isArray(newStudents) || newStudents.some(student => typeof student !== 'string' || student.trim().length === 0)) {
         throw new Error('Students must be an array with non empty strings');
     }
