f73c1e498e6e28e74d39469328c8e3ae39c43f30
chdrchz <saavannaa.rose@gmail.com>
2024-05-03 17:50:28 -0500

    adding currency class with constructor method, getters and setters, and a function that displays the specified currency type formatted

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,45 @@
+export default class Currency {
+  // constructor method for obj creation
+  constructor(code, name) {
+    // verify code
+    if (typeof code !== 'string' || code.trim().length === 0) {
+      throw new Error('Code must be a non empty string');
+    }
+    // verify name
+    if (typeof name !== 'string' || name.trim().length === 0) {
+      throw new Error('Name must be a non empty string');
+    }
+
+    this._code = code;
+    this._name = name;
+  }
+
+  // getters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // setters
+  set code(newCode) {
+    if (typeof newCode !== 'string' || newCode.trim().length === 0) {
+      throw new Error('Code must be a non empty string');
+    }
+    this._code = newCode;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string' || newName.trim().length === 0) {
+      throw new Error('Name must be a non empty string');
+    }
+    this._name = newName;
+  }
+
+  // displays formatted currency type ie: Dollars ($)
+  displayFullCurrency(name, code) {
+    return (`${this.name} (${this.code})`);
+  }
+}
