43829379e686b19da60818b9b84a6a133b9cdf0e
chdrchz <saavannaa.rose@gmail.com>
2024-05-05 11:06:08 -0500

    semistandard fixes

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,13 @@
 export default class Building {
-    constructor(sqft) {
-      // any class extending Building must implement below method
-      if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
-  
-      this._sqft = sqft;
-    }
-  
-    // Getters
-    get sqft() {
-      return this._sqft;
-    }
+  constructor (sqft) {
+    // any class extending Building must implement below method
+    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
+
+    this._sqft = sqft;
   }
+
+  // Getters
+  get sqft () {
+    return this._sqft;
+  }
+}
