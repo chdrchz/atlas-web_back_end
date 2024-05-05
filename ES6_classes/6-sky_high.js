82d19d907d5ad328d55aa368d1364094c41d9033
chdrchz <saavannaa.rose@gmail.com>
2024-05-05 11:20:12 -0500

    adding class that extends another, and has a method to print an evacuation message, also semistandard

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof floors !== 'number' || floors <= 0) {
+      throw new Error('Floors must be a positive, non-zero number');
+    }
+
+    this._floors = floors;
+  }
+
+  // getters
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  // evacuation message method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
