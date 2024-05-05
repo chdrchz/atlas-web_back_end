d11c086a5833e38be2c3c535f497c139a890df08
chdrchz <saavannaa.rose@gmail.com>
2024-05-05 11:05:13 -0500

    testing simplified version

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,25 +1,13 @@
 export default class Building {
-  // constructor method
-  constructor(sqft) {
-    // verify class
-    if (new.target === Building) {
-      throw new Error('Abstract classes cannot be instantiated directly');
+    constructor(sqft) {
+      // any class extending Building must implement below method
+      if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
+  
+      this._sqft = sqft;
     }
-
-    // verify sqft
-    if (typeof sqft !== 'number' || sqft < 0) {
-      throw new Error('Sqft must be a positive or zero number');
+  
+    // Getters
+    get sqft() {
+      return this._sqft;
     }
-    this._sqft = sqft;
   }
-
-  // getters
-  get sqft() {
-    return this._sqft;
-  }
-
-  // abstract method
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-}
