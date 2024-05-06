12f75cbe16cdc475211662b9939aa43a9b15441d
chdrchz <saavannaa.rose@gmail.com>
2024-05-06 11:49:28 -0500

    adding class that gets the location and location symbol, also semistandard

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,54 @@
+export default class HolbertonClass {
+  // constructor
+  constructor(size, location) {
+    // verify size
+    if (typeof size !== 'number' || size <= 0) {
+      throw new Error('Size must be a positive, non-zero number');
+    }
+
+    // verify location
+    if (typeof location !== 'string' || location.length <= 0) {
+      throw new Error('Location must be a non-empty string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  // getters
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  // setters
+  set size(newSize) {
+    if (typeof newSize !== 'number' || newSize <= 0) {
+      throw new Error('Size must be a non-zero number');
+    }
+    this._size = newSize;
+  }
+
+  set location (newLocation) {
+    if (typeof newLocation !== 'string' || newLocation.length <= 0) {
+      throw new Error('Location must be a non empty string');
+    }
+    this._location = newLocation;
+  }
+
+  // string representation
+  [Symbol.toPrimitive](type) {
+    if (type === 'string') {
+      return this._location;
+    }
+    if (type === 'number') {
+      return this._size;
+    }
+    if (type === 'undefined') {
+      throw new Error('Type must be a valid number or string');
+    }
+  }
+}
