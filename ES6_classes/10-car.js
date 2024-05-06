27e5fa1abe881a560c21b78f4115340fde619b1f
chdrchz <saavannaa.rose@gmail.com>
2024-05-06 12:20:27 -0500

    adding class car that takes multiple parameters, and a method that returns an object of the class, also semistandard

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,62 @@
+export default class Car {
+  // constructor method
+  constructor(brand, motor, color) {
+    // verify brand
+    if (typeof brand !== 'string' || brand.length <= 0) {
+      throw new Error('Brand must be a non empty string');
+    }
+
+    // verify motor
+    if (typeof motor !== 'string' || motor.length <= 0) {
+      throw new Error('Motor must be a non empty string');
+    }
+
+    // verify color
+    if (typeof color !== 'string' || color.length <= 0) {
+      throw new Error('Color must be a non empty string');
+    }
+
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // getters
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  // setters
+  set brand(newBrand) {
+    if (typeof newBrand !== 'string' || newBrand.length <= 0) {
+      throw new Error('Brand must be a non empty string');
+    }
+    this._brand = newBrand;
+  }
+
+  set motor(newMotor) {
+    if (typeof newMotor !== 'string' || newMotor.length <= 0) {
+      throw new Error('Motor must be a non empty string');
+    }
+    this._motor = newMotor;
+  }
+
+  set color(newColor) {
+    if (typeof newColor !== 'string' || newColor.length <= 0) {
+      throw new Error('Color must be a non empty string');
+    }
+    this._color = newColor;
+  }
+
+  static cloneCar(brand, motor, color) {
+    return new Car(brand, motor, color);
+  }
+}
