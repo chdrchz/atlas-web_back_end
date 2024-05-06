7411b94fcc33432fc5892ddfc7d8e30c2cc3ef45
chdrchz <saavannaa.rose@gmail.com>
2024-05-06 13:59:40 -0500

    fixing typos

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-    const studentOne = { firstName: "Guillaume", id: 1, location: 'San Fransisco' };
+    const studentOne = { firstName: "Guillaume", id: 1, location: 'San Francisco' };
     const studentTwo = { firstName: "James", id: 2, location: 'Columbia' };
-    const studentThree = { firstName: "Serena", id: 5, location: 'San Fransisco' };
+    const studentThree = { firstName: "Serena", id: 5, location: 'San Francisco' };
 
     const studentArray = {studentOne, studentTwo, studentThree};
     return studentArray;
