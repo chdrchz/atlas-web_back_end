7d749338c8c4ce3954a202dacb972d90aa84595e
chdrchz <saavannaa.rose@gmail.com>
2024-05-06 14:03:44 -0500

    linter fixes

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
-    const studentOne = { firstName: "Guillaume", id: 1, location: 'San Francisco' };
-    const studentTwo = { firstName: "James", id: 2, location: 'Columbia' };
-    const studentThree = { firstName: "Serena", id: 5, location: 'San Francisco' };
+  const studentOne = { firstName: 'Guillaume', id: 1, location: 'San Francisco' };
+  const studentTwo = { firstName: 'James', id: 2, location: 'Columbia' };
+  const studentThree = { firstName: 'Serena', id: 5, location: 'San Francisco' };
 
-    const studentArray = [studentOne, studentTwo, studentThree];
-    return studentArray;
+  const studentArray = [studentOne, studentTwo, studentThree];
+  return studentArray;
 }
