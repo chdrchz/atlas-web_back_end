c055d3e06e976e0893316b04b59834d5d5719740
chdrchz <saavannaa.rose@gmail.com>
2024-08-26 13:22:48 -0500

    adding function that statically types the 2 string variables, and concatenates them into a string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Function that takes str1 and str2 as args and returns a concatenated string
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that takes str1 and str2 as args and returns a concatenated string
+    """
+    
+    return str1 + str2
