55555d6132d45e0320f98f9f6b5abd1d3cbbfb3b
chdrchz <saavannaa.rose@gmail.com>
2024-08-26 14:47:19 -0500

    str represenatation of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function that takes a float as arg and returns the str
+representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that takes a float as arg and returns the str
+    representation of the float
+    """
+    return n;
