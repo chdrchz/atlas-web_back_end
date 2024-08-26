e1063593dfdd5034c0e9789ce2550b8acf8013b8
chdrchz <saavannaa.rose@gmail.com>
2024-08-26 13:32:18 -0500

    chmodding and also adding function that returns the floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Function that returns the floor of a floating integer
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function that returns the floor of a floating integer
+    """
+
+    return math.floor(n)
