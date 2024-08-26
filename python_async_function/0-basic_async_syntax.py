6e4526af51663c164d92d76affd8defa5dbddb94
chdrchz <saavannaa.rose@gmail.com>
2024-08-26 15:04:44 -0500

    another line???

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -12,6 +12,7 @@ async def wait_random(max_delay: int = 10) -> float:
     Coroutine that takes a max_delay as an argument, and randomly generates
     a number to wait to complete the asynchronous operation.
     """
+
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
