0b68be1b2456b1377aa6501d38dcfd8afc7896fa
chdrchz <saavannaa.rose@gmail.com>
2024-08-26 14:29:38 -0500

    This function takes a float and returns a function that multiplies the original argument with itself

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+This function takes a float and returns a function that multiplies 
+the original argument with itself 
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    This function takes a float and returns a function that multiplies 
+    the original argument with itself 
+    """
+    def multiplier_function(val: float) -> float:
+        """
+        The multiplier function that multiplies multiplier with itself
+        """
+        return val * multiplier
+    
+    return multiplier_function
