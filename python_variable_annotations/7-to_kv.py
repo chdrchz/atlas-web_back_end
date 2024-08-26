e8eb03074716fe363bd628e262f4760f9ce46404
chdrchz <saavannaa.rose@gmail.com>
2024-08-26 14:20:48 -0500

    comment was too long

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 """
-Function that returns a tuple containing the string and the square of v (float or int)
+Function that returns a tuple containing the string and the square of v.
 """
 from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Function that returns a tuple containing the string and the square of v (float or int)
+    Function that returns a tuple containing the string and the square of v.
+    v can be either ints or floats
     """
 
     return (k, v ** 2.0)
