024bba4bedd3699d440f97c830e3ea056f953d97
chdrchz <saavannaa.rose@gmail.com>
2024-08-26 15:00:33 -0500

    Coroutine that takes a max_delay as an argument, and randomly generates  a number to wait to complete the asynchronous operation.

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Coroutine that takes a max_delay as an argument, and randomly generates
+ a number to wait to complete the asynchronous operation.
+"""
+import asyncio, random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Coroutine that takes a max_delay as an argument, and randomly generates
+    a number to wait to complete the asynchronous operation.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
