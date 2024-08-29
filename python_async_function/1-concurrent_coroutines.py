5c0fd5170e226c66f9b426a94f3b068432376ea7
chdrchz <saavannaa.rose@gmail.com>
2024-08-29 14:23:34 -0500

    making things executable and also adding coroutine that imports a function and calls it n times and calulates delay between each call

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Coroutine to call imported function n times and calculate
+delay between each
+"""
+
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]: 
+    """
+    Coroutine to call imported function n times and calculate
+    delay between each
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+
+    delays = []
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
