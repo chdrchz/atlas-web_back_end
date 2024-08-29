3aaea6ca8c75d7e6ba3413f228f1f323bec0f311
chdrchz <saavannaa.rose@gmail.com>
2024-08-29 14:39:57 -0500

    removing whitespace

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]: 
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Coroutine to call imported function n times and calculate
     delay between each
