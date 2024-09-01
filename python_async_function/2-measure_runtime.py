49b6a3fe5f5f49a5895139ab218daa48108f92f3
chdrchz <saavannaa.rose@gmail.com>
2024-09-01 11:37:50 -0500

    adding function that calculates the total time taken to perform a single task

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to calculate elapsed time
+"""
+
+import time
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Calculates elapsed time.
+
+    Args:
+        n: any int > 0
+        max_delay: any int > 0
+    
+    Return: 
+        total_time / n as a float
+    """
+
+    # Start the timer
+    start_time = time.perf_counter()
+
+    # Properly await the coroutine
+    await wait_n(n, max_delay)
+
+    # Calculate the total elapsed time
+    total_time = time.perf_counter() - start_time
+
+    # Return the average time per task
+    return total_time / n
