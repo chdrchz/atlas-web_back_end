caaa9a4c93c255faa1265018e9611473378344c4
chdrchz <saavannaa.rose@gmail.com>
2024-09-01 12:28:31 -0500

    documentation

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module that contains a function to calculate elapsed time
+Module that contains a function to return a Task object
 """
 
 import time
@@ -10,6 +10,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Returns wait_random as an individual task
 
+    Args:
+        max_delay: any int > 0
+    
+    Return: 
+        wait_random as an asyncio.Task object
+    """
     # Returns the wait_random coroutine as a manipulatable task
     return asyncio.create_task(wait_random(max_delay))
