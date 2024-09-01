8225c244eddc1c0662295ba451744807c45eb552
chdrchz <saavannaa.rose@gmail.com>
2024-09-01 12:35:10 -0500

    swapping wait_random and wait_n to task_wait_random and task_wait_n

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function call imported function 
+n times and calculate the delay between each
+"""
+from typing import List
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Coroutine to call imported function n times and calculate
+    delay between each
+
+    Args:
+        n: any int > 0
+        max_delay: any int > 0
+
+    Return:
+        list of floats
+    """
+
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    delays = []
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
