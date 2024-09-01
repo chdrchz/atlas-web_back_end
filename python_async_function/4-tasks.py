d3ec1eed844845679a3555817a83f6c706ae764c
chdrchz <saavannaa.rose@gmail.com>
2024-09-01 12:38:04 -0500

    py style fixes

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-Module that contains a function call imported function 
+Module that contains a function call imported function
 n times and calculate the delay between each
 """
 from typing import List
 import asyncio
 
 task_wait_random = __import__('3-tasks').task_wait_random
+
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
@@ -21,10 +22,13 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
         list of floats
     """
 
+    # Calling task_wait_random n times
     tasks = [task_wait_random(max_delay) for _ in range(n)]
 
+    # Empty list for delays
     delays = []
 
+    # Iterate over all tasks and calculate the delay
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
