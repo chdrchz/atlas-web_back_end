d8376821d76dded7179a496faf90b68a24277872
chdrchz <saavannaa.rose@gmail.com>
2024-09-02 12:13:54 -0500

    adding class that inherits from BaseCaching to add and retrieve from a dictionary. This time with FIFO principles

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ FIFOCaching defines:
+        - adds an item to cache_data, utilizes FIFO
+        - retrieves an item from cache_data
+    """
+    
+    def __init__(self):
+        """ Initialization from BaseCaching
+        """
+        super().__init__()
+    
+    def put(self, key, item):
+        """ Adds an item to cache_data
+
+            Args:
+            - self: Instance of class
+            - key: key for item
+            - item: item to be added to cache_data
+            
+            Return:
+            - None
+        """
+        
+        # Prevent empty values from being added to cache_data
+        if key == "":
+            return
+        
+        # Add item to cache_data, based on its key
+        self.cache_data[key] = item
+        
+        # Remove the first item if the dictionary is greater than 4
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # iter() is like a loop, creates an iterable object
+            first_key = next(iter(self.cache_data))
+            first_item = self.cache_data.pop(first_key)
+            print(f'DISCARD: {first_item}')
+
+    def get(self, key):
+        """ Gets an item from cache_data
+
+            Args:
+            - self: Instance of class
+            - key: key for item
+            
+            Return:
+            - item from cache_data, based on key
+            - None if key is none, or if key doesn't exist
+        """
+        
+        # If they key exists
+        if self.cache_data[key]:
+            if key == None:
+                return None
+            
+            # Retrieve item in cache_data, based on its key
+            return self.cache_data.get(key)
+        
+        else: 
+            return None
