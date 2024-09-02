3056c6152bf1d4fd47794029b16826e817f57298
chdrchz <saavannaa.rose@gmail.com>
2024-09-02 11:26:04 -0500

    adding basicCache class, inherits from baseCaching and defines a put and get method based on key

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCaching defines:
+        - adds an item to cache_data
+        - retrieves an item from cache_data
+    """
+    def put(self, key, item):
+        """ Adds an item to cache_data
+
+            Args:
+            - self: Instance of class
+            - key: key for item
+            - item: item to be added to cache_data
+            
+            Return:
+            - None
+        """
+
+        # Prevent empty values from being added to cache_data
+        if key == "":
+            return
+
+        # Add item to cache_data, based on its key
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Gets an item from cache_data
+
+            Args:
+            - self: Instance of class
+            - key: key for item
+            
+            Return:
+            - item from cache_data, based on key
+        """
+        
+        # Retrieve item in cache_data, based on its key
+        return self.cache_data.get(key)
+            
+            
+    
