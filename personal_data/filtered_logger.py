0f65cba903d3171f5683ca139fcbb6f7458e8dd0
chdrchz <saavannaa.rose@gmail.com>
2024-09-08 11:28:30 -0500

    adding class that formats log message

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
-""" Module that contains a function that returns an
-    obfuscated log message.
+""" Module that obfuscates data
+
+    Functions:
+        - filter_datum: Obfuscates field data and
+        returns a string with redacted Personal Data
+    
+    Classes: 
+        - RedactingFormatter: Filters data
 """
 
 import re
+import logging
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor method
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format log message by redacting specific fields
+        """
+        log_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, log_message, self.SEPARATOR)
 
 
 def filter_datum(
