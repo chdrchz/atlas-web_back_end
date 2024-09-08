dd7e9f31d784209ac42031cddaaef1f0435498d2
chdrchz <saavannaa.rose@gmail.com>
2024-09-08 11:16:59 -0500

    pycode

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,18 +7,20 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+    ) -> str:
     """ Function that obfuscates data fields.
-    
-        Args: 
+
+        Args:
             - fields: the value type of data to be osfucated
             - redaction: what to replace the field data with
             - message: the string to parse
             - seperator: what each field is separated by to parse
-        
+
         Return:
             - the message obfuscated
     """
     pattern = r'(' + '|'.join(fields) + r')=([^' + re.escape(separator) + r']*)'
-    
+
     return re.sub(pattern, lambda match: match.group(1) + '=' + redaction, message)
