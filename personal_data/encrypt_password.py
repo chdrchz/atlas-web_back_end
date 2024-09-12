52b978bb6a58a54c65788cc2ee094f7e46f74f01
chdrchz <saavannaa.rose@gmail.com>
2024-09-12 16:44:06 -0500

    documentation and pycode style fixes

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-""" Module that contains a function to
-    hash + salt passwords
+""" Module that hashes and salts passwords,
+    and checks against them to verify login state
+    
+    Functions: 
+        - hash_password():
+            hashes and salts a string (password)
+        - is_valid():
+            returns a boolean if the hashed passwords match
 """
 
 import bcrypt
@@ -8,6 +14,12 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """ Function that hashes and salts a password
+
+        Args:
+            - password: the password to hash and salt
+
+        Return:
+            - hashed + salted password in bytes
     """
     bytes = password.encode('utf-8')
 
@@ -17,23 +29,24 @@ def hash_password(password: str) -> bytes:
 
     return hash
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Function that checks if a password 
+    """ Function that checks if a password
         matches a hashed password
-        
+
         Args:
             - hashed_password: the hashed password from hash_password()
             - password: the password to be checked against
-        
-        Return: 
+
+        Return:
             - Boolean:
                 - True if the password matches the hashed_password
                 - False if the password doesn't match the hashed_password
     """
     # First of all, hash that pass!
     password_bytes = password.encode('utf-8')
-    
+
     # Check if the password matches
     result = bcrypt.checkpw(password_bytes, hashed_password)
-    
+
     return result
