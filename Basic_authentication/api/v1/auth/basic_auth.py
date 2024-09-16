0eab66c63bd507477fdfaf07b174827ff116386e
chdrchz <saavannaa.rose@gmail.com>
2024-09-16 12:11:50 -0500

    adding function that uses string splicing to return the base 64 part of the string

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """ Empty AH class
+
+    Functions:
+        - def extract_base64_authorization_header(self, authorization_header: str) -> str:
 """
 
 from api.v1.auth.auth import Auth
@@ -8,4 +11,25 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ This will do something in the future
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Function that 
+        """
+        # No header is provided
+        if authorization_header is None:
+            return None
+
+        # Header must be a valid string
+        if not isinstance(authorization_header, str):
+            return None
+
+        header = "Basic "
+
+        # Header must start with Basic
+        if authorization_header.startswith(header):
+
+            # Calculate the position at the end of header
+            start_index = len(header)
+            return authorization_header[start_index:]
+        else:
+            return None
