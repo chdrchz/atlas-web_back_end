506d39d45965b8588569cf696b6951c512ee01e3
chdrchz <saavannaa.rose@gmail.com>
2024-09-16 10:58:32 -0500

    added an import

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -9,6 +9,7 @@
         - def forbidden(error) -> str: Throws a 403 error
 """
 from os import getenv
+from api.v1.auth.auth import Auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
