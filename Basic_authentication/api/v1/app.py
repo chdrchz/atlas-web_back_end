9e8cfe2351d0d294431716fb2a90f45a02775f27
chdrchz <saavannaa.rose@gmail.com>
2024-09-16 10:31:12 -0500

    pycode style

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -21,20 +21,21 @@ if auth_type:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request_handler():
     """ This function will be executed before each request
     """
     if auth is None:
         return
-    
+
     # No authentication needed for these
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
     ]
-    
+
     # Is the path in public paths?
     if request.path in excluded_paths:
         return
@@ -43,9 +44,9 @@ def before_request_handler():
     if request.path not in excluded_paths and \
             auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
-            abort(401) # Unauthorized
+            abort(401)  # Unauthorized
         if auth.current_user(request) is None:
-            abort(403) # Forbidden
+            abort(403)  # Forbidden
 
 
 @app.errorhandler(404)
