04ad4a4667e9dec17365833d5b37475799a8c467
chdrchz <saavannaa.rose@gmail.com>
2024-09-18 12:28:55 -0500

    adding session auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module that contains a class to handle session authentication
+
+    Functions:
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class
+    """
+    pass
