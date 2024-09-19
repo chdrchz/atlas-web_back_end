0e285a0cee6365866be535da506eb385fba9108a
chdrchz <saavannaa.rose@gmail.com>
2024-09-19 11:38:23 -0500

    testiing this bad boy out

---
+++ Session_authentication/api/v1/app.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+""" Route module for the API
+
+    Functions:
+        - before_request_handler(): Runs before everythign else,
+            and handles authorizing paths
+        - def not_found(error) -> str: Throws a 404 error
+        - def unauthorized(error) -> str: Throws a 401 error
+        - def forbidden(error) -> str: Throws a 403 error
+"""
+from os import getenv
+from api.v1.auth.auth import Auth
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# Initialize auth variable
+auth = None
+
+# Check environment variable and configure authentication
+# based on auth_type
+auth_type = os.getenv('AUTH_TYPE')
+
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
+
+@app.before_request
+def before_request_handler():
+    """ Authorizing access based on paths
+    """
+    if auth is None:
+        return
+
+    # No authentication needed for these
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+
+    # Is the path in public paths?
+    if request.path in excluded_paths:
+        return
+
+    # Do auth stuff
+    if request.path not in excluded_paths and \
+            auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)  # Unauthorized
+    
+    request.current_user = auth.current_user(request)
+
+    if request.current_user is None:
+        abort(403)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Not authorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+# Define a TypeVar for User
+
+
+class Auth():
+    """ Template class for all authentication systems.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method that searches the path
+
+            Args:
+                - self
+                - path: The endpoint or URL path of the incoming request
+                - excluded_paths: Paths that don't need authentication
+
+            Return:
+                - True: If the path does not exist in excluded paths,
+                    or if path is none
+                - False: If it does exist in excluded_paths
+        """
+
+        # No authentication required
+        if path is None:
+            return True
+
+        # No authentication needed since there is nothing to exclude
+        if not excluded_paths:
+            return True
+
+        # Normalize trailing slash
+        if not path.endswith('/'):
+            path += '/'
+
+        # If the path matches the path in excluded paths
+        for excluded in excluded_paths:
+            if path.endswith('/') and path == excluded:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Locates the authorization header
+
+            Args:
+                - self
+                - request: the header to retrieve
+
+            Return:
+                - The header retrieved in string format
+        """
+        if request is None:
+            return None
+
+        # Returns None if Authorization isn't found,
+        # or the header itself if it is
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        This method will later return the current user.
+        Currently, it returns None as a placeholder.
+        """
+        return None
