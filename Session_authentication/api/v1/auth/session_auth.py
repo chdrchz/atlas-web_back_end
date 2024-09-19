8ceba6881f27527e9247c44a410a499ab895532c
chdrchz <saavannaa.rose@gmail.com>
2024-09-19 15:59:30 -0500

    pycodestyle

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -29,6 +29,6 @@ class SessionAuth(Auth):
             session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
-        
+
         # User_id is not a valid, non-empty string
         return None
