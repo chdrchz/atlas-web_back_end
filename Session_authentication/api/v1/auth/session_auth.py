6bb0cc504f298d611d55933e561c8cee13195a40
chdrchz <saavannaa.rose@gmail.com>
2024-09-19 14:56:31 -0500

    fixed the dictionary name

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """_summary_
 """
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -11,3 +12,22 @@ class SessionAuth(Auth):
     Args:
         Auth (_type_): _description_
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """_summary_
+
+        Args:
+            user_id (str, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+
+        try:
+            if user_id is not None or isinstance(user_id, str):
+                # Create a session
+                self.session_id = str(uuid.uuid4())
+                self.user_id_by_session_id[self.session_id] = user_id
+        except Exception as e:
+            return None
