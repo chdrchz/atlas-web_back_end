b1eaf7f9e79b93ce7157c85af6a820d0433cdbc8
chdrchz <saavannaa.rose@gmail.com>
2024-09-20 12:28:05 -0500

    I was throwing the wrong error completely

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -23,30 +23,31 @@ def login() -> str:
     if email:
         users = User.search({"email": email})
         if users:
+            
             user = users[0]
+            # If the User exists, try to authenticate the password
+            if not user.is_valid_password(password):
+                return jsonify({"error": "wrong password"}), 400
 
+            # The user is authenticated with password, create a session
+            else:
+                from api.v1.app import auth
+                
+                # Create the session
+                session_id = auth.create_session(user.id)
+                
+                # Grab that data from User
+                response = make_response(user.to_json())
+                
+                # Set the cookie for the user
+                response.set_cookie(os.getenv(
+                    'SESSION_NAME', '_my_session_id'), session_id)
+                return response
+        
         # User was not found
         else:
-            return jsonify({"error": "no user found for this email"}), 404
+            return jsonify({"error": "no user found for this email"}), 404   
 
     # Email was not found
     else:
         return jsonify({"error": "email missing"}), 400
-    
-    # If the User exists, try to authenticate the password
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    
-    # The user is authenticated with password, create a session
-    from api.v1.app import auth
-
-    # Create the session
-    session_id = auth.create_session(user.id)
-
-    # Grab that data from User
-    response = make_response(user.to_json())
-                
-    # Set the cookie for the user
-    response.set_cookie(os.getenv(
-        'SESSION_NAME', '_my_session_id'), session_id)
-    return response
