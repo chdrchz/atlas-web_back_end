1d194d6f8464a711eeeb4e58c929f1aaf049010e
chdrchz <saavannaa.rose@gmail.com>
2024-09-22 11:48:29 -0500

    echo=true needs to get OUTTA here

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -36,5 +36,4 @@ class DB:
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
-        self._session.refresh(new_user)
         return new_user
