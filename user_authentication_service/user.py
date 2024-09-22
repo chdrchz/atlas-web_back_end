dc4e771940a7c405122fee3c6ac241037766e5c8
chdrchz <saavannaa.rose@gmail.com>
2024-09-22 11:22:57 -0500

    adding class that adds user data to the sql database

---
+++ user_authentication_service/user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module that contains a User class
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Base class creation to map python classes to database tables
+Base = declarative_base()
+
+# Define a model that maps to a table
+class User(Base):
+    """ Class that gets the 
+
+    Args:
+        Base (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    # The name of the table in the database
+    __tablename__ = 'users'
+
+    # Save the user's data in the table
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(255), nullable=False, unique=True)
+    hashed_password = Column(String(255), nullable=False)
+    session_id = Column(String(255), nullable=True)
+    reset_token = Column(String(255), nullable=True)
