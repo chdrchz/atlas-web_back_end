41681a0e01908c098ab6011bbc724fc1e84918d9
chdrchz <saavannaa.rose@gmail.com>
2024-09-22 13:12:30 -0500

    adding the users endpoint

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+# Set the auth_type
+AUTH = Auth()
 
 
 @app.route('/',  methods=['GET'])
@@ -11,6 +15,25 @@ def hello():
     """
     return jsonify({"message": "Bienvenue"})
 
+
+@app.route('/users',  methods=['POST'])
+def users(email, password):
+    """ Finds the user based on email and password
+
+    Args:
+        email (_type_): Email for user
+        password (_type_): Password for user
+
+    Returns:
+        200 or 400
+    """
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
