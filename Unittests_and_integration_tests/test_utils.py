1f2a33a607168154e647873be420a5e0db257475
chdrchz <saavannaa.rose@gmail.com>
2024-09-23 09:49:16 -0500

    addinmg test to check if it is proeprly raising a key errpr

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -18,10 +18,27 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """ Tests the access_nested_map function
+            for expected vs actual
 
         Args:
             nested_map (_type_): Data to be iterated over
-            path (_type_): Specific path to serarch in the nested map
+            path (_type_): Specific path to search in the nested map
             expected (_type_): The expected result
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+    
+    @parameterized.expand([
+        ({}, ("a",), "Key 'a' not found in nested map"),
+        ({"a": 1}, ("a", "b"), "Key 'b' not found in nested map"),
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map, path, expected_exception_message):
+        """ Tests the access_nested_ap function for key
+
+        Args:
+            nested_map (_type_): Data to be iterated over
+            path (_type_): Specific path to search in the nested map
+            expected_exception_message (_type_): "Key 'a' not found in nested map"
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
