f4ddaa9e0697bd6b2d74afd98546d3e0521580dc
chdrchz <saavannaa.rose@gmail.com>
2024-09-25 16:10:15 -0500

    using patch to mock a payload request

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import PropertyMock, patch
 from client import GithubOrgClient
 from parameterized import parameterized
 
@@ -19,3 +19,26 @@ class TestGithubOrgClient(unittest.TestCase):
         client.org
 
         mock_get.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        mock_repos_payload = [{'name': 'repo1'}, {'name': 'repo2'}]
+        mock_get_json.return_value = mock_repos_payload
+        mock_public_repos_url = 'http://mocked.url/repos'
+
+        with patch.object(GithubOrgClient,
+                          '_public_repos_url',
+                          new_callable=PropertyMock) as mock_repo_url:
+            mock_repo_url.return_value = mock_public_repos_url
+
+            github_org_client = GithubOrgClient("some_org")
+
+            # Get org repos
+            public_repos = github_org_client.public_repos()
+
+            # Check that the repo matches the payload
+            self.assertEqual(public_repos,
+                             [repo['name'] for repo in mock_repos_payload])
+
+            mock_repo_url.assert_called_once()
+            mock_get_json.assert_called_once_with(mock_public_repos_url)
