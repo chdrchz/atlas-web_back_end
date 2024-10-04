c3461cb5bbfddd389eac2d7bad9832726e0e09f1
chdrchz <saavannaa.rose@gmail.com>
2024-10-04 10:52:25 -0500

    adding a decorator function for store that counts the number of times methiods from the cache class are called.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,9 +2,31 @@
 """ Module that contains a class Cache
 """
 
-import redis
 import uuid
+import redis
+from functools import wraps
 from typing import Callable, Optional, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    """_summary_
+
+    Args:
+        method (Callable): _description_
+
+    Returns:
+        Callable: _description_
+    """
+    # Key is qualifiable
+    key = method.__qualname__
+    
+    # Count how many times the method is called
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache: 
@@ -20,6 +42,7 @@ class Cache:
         # Flush the database
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Generates a random uuid and stores the key in redis
 
