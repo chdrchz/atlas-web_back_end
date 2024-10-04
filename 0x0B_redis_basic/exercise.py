c325348d0c592b86353e169e58d76a797c939495
chdrchz <saavannaa.rose@gmail.com>
2024-10-04 10:58:16 -0500

    removing self?

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -23,11 +23,11 @@ def count_calls(method: Callable) -> Callable:
     
     # Count how many times the method is called
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
+    def wrapper(*args, **kwargs):
         
         # Same as i++
         self._redis.incr(method.__qualname__)
-        return method(self, *args, **kwargs)
+        return method(*args, **kwargs)
 
     return wrapper
 
