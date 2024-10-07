56c288a2f9af8e7f3bb07cc04a6ac04bff1e3596
chdrchz <saavannaa.rose@gmail.com>
2024-10-07 13:57:08 -0500

    cxalculating average score

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- STUFF
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    -- Calculate and store the average score
+    SELECT AVG(score) INTO avg_score
+    FROM corrections WHERE user_id = user_id;
+
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+
+END //
+
+DELIMITER ;
