fa63804eaf814d3a6b1c9b27a4612474178bbcaa
chdrchz <saavannaa.rose@gmail.com>
2024-10-07 13:03:46 -0500

    query to reset the valid_email variable if an email has been updated

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Resets the valid_email variable if the email is updated
+DELIMITER //
+
+CREATE TRIGGER reset_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END;
+
+//
