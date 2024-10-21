b2b765a78c44b484e90c05b83d9597045e12cd68
chdrchz <saavannaa.rose@gmail.com>
2024-10-21 11:56:59 -0500

    spacing

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -8,17 +8,19 @@ function calculateNumber(type, a, b) {
     // If type is not a valid type
     if (!allowedTypes.includes(type)) {
         console.log("Type is not valid. Must be either 'SUM', 'SUBTRACT', 'DIVIDE'. Re-enter Type.");
-        return null; // Return null or handle the error as needed
+        return null;
     }
 
     // If type is SUM
     if (type === allowedTypes[0]) {
         return Math.round(a) + Math.round(b);
     }
+
     // If type is SUBTRACT
     else if (type === allowedTypes[1]) {
         return Math.round(a) - Math.round(b);
     }
+
     // If type is DIVIDE
     else if (type === allowedTypes[2]) {
         if (Math.round(b) !== 0) {
