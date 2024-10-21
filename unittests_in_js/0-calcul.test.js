8e811c47e9ed6b7064a21c418efe8d6944ecf1ab
chdrchz <saavannaa.rose@gmail.com>
2024-10-21 11:01:36 -0500

    maybe no strictequal?

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,18 +4,18 @@ const calculateNumber = require('./0-calcul.js')
 // Test Suite
 describe('calculateNumber', function() {
     it ('should return 4 when args are 1 and 3', function() {
-        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.Equal(calculateNumber(1, 3), 4);
     });
 
     it ('should return 5 when args are 1 and 3.7', function() {
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.Equal(calculateNumber(1, 3.7), 5);
     });
 
     it ('should return 5 when args are 1.2 and 3.7', function() {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.Equal(calculateNumber(1.2, 3.7), 5);
     });
 
     it ('should return 6 when args are 1.5 and 3.7', function() {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+        assert.Equal(calculateNumber(1.5, 3.7), 6);
     });
 })
