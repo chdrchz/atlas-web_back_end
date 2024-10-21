a11fd6c08be6385a53ad1157b4dbddd9161259e7
chdrchz <saavannaa.rose@gmail.com>
2024-10-21 11:32:03 -0500

    me prefer arrow functions

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js')
 
 // Test Suite
-describe('calculateNumber', function() {
+describe('calculateNumber', () => {
     it ('should return 4 when args are 1 and 3', function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
     });
