804fde6017f8283e2cd812a2f4e826bd01cea608
chdrchz <saavannaa.rose@gmail.com>
2024-10-22 15:46:10 -0500

    need to use before and afterEach

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,8 +1,19 @@
 const chai = require("chai");
+const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./4-payment.js");
 const { expect } = chai;
 
 describe("sendPaymentRequestToApi", () => {
+    beforeEach(() => {
+        // Create a stub for calculateNumber and force it to return 10
+        spy = sinon.spy(Utils, "calculateNumber").returns(10);
+      });
+    
+      afterEach(() => {
+        // Restore the original function after each test
+        spy.restore();
+      });
+
     it("should call Utils.calculateNumber with SUM and arguments 100, 20", () => {
         result = sendPaymentRequestToApi(100, 20);
         console.log(`The total is: ${result}`);
