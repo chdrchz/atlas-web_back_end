1de6401b6090d11f52877ff2718017459c1bf924
chdrchz <saavannaa.rose@gmail.com>
2024-10-22 10:26:32 -0500

    using chai and expect instead of assert

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber()', () => {
+  it('should return the sum of rounded a and rounded b', () => {
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+    expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+  });
+
+  it('should return the dividend of rounded a divided by rounded b', () => {
+    expect(calculateNumber('DIVIDE', 4, 1.7)).to.equal(2);
+    expect(calculateNumber('DIVIDE', 2.2, 1)).to.equal(2);
+    expect(calculateNumber('DIVIDE', 10.1, 10.1)).to.equal(1);
+    expect(calculateNumber('DIVIDE', 100, 0)).to.equal('Error');
+  });
+
+  it('should return the difference between rounded a minus rounded b', () => {
+    expect(calculateNumber('SUBTRACT', 6, 2.8)).to.equal(3);
+    expect(calculateNumber('SUBTRACT', 5, 2)).to.equal(3);
+    expect(calculateNumber('SUBTRACT', 11, 5.4)).to.equal(6);
+  });
+});
